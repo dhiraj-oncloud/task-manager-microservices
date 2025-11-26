a5b8ee3ab9ca75a9a559f5600f0fe6eca1a6f295
Dhiraj Kumar Mishra <dhirajmishra.awscloud@gmail.com>
2025-11-26 23:57:15 +0530

    Update index.js

--- user-service/index.js
+++ user-service/index.js
@@ -1,10 +1,15 @@
 const express = require('express');
 const app = express();
+const port = 3000;
 
-app.get('/health', (req, res) => {
-  res.json({ status: 'UP' });
+app.get('/', (req, res) => {
+  res.send('Hi all friends.My first cicd learning');
 });
 
-app.listen(3000, () => {
-  console.log('Server running on port 3000');
+app.get('/health', (req, res) => {
+  res.status(200).json({ status: 'UP' });
+});
+
+app.listen(port, () => {
+  console.log(`Server running on port ${port}`);
 });
